5a7b97bb6ac971f93c8e38d0e769e3cf3d5f0661
Sonia Caldas <caldasgsonia@gmail.com>
2024-08-01 09:53:25 -0300

    adding the service layer

--- crud-spring/src/main/java/com/loiane/model/SpringVersionChecker.java
+++ crud-spring/src/main/java/com/loiane/model/SpringVersionChecker.java
@@ -1,12 +1,12 @@
-////This will just give you which Spring version you are using :)
-////(If you check the pom.xml file, there you'll find only the Spring Boot version)
+//This will just give you which Spring version you are using :)
+//(If you check the pom.xml file, there you'll find only the Spring Boot version)
 
-// package com.loiane.model;
+package com.loiane.model;
 
-// import org.springframework.core.SpringVersion;
+import org.springframework.core.SpringVersion;
 
-// public class SpringVersionChecker {
-//     public static void main(String[] args) {
-//         System.out.println("Spring Version: " + SpringVersion.getVersion());
-//     }
-// }
+public class SpringVersionChecker {
+    public static void main(String[] args) {
+        System.out.println("Spring Version: " + SpringVersion.getVersion());
+    }
+}
---
+++ crud-spring/src/main/java/com/loiane/service/CheesesService.java
@@ -0,0 +1,8 @@
+package com.loiane.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheesesService {
+        
+}
