231ae41db8afeb55d7e57792450bd6dd6e0deb98
Sonia Caldas <caldasgsonia@gmail.com>
2024-08-07 10:48:12 -0300

    deleted comments, everything working beautifully

--- crud-spring/src/main/java/com/loiane/enums/validation/ValueOfEnumValidator.java
+++ crud-spring/src/main/java/com/loiane/enums/validation/ValueOfEnumValidator.java
@@ -1,32 +1,3 @@
-// package com.loiane.enums.validation;
-
-// import java.util.List;
-// import java.util.stream.Collectors;
-// import java.util.stream.Stream;
-
-// import jakarta.validation.ConstraintValidator;
-// import jakarta.validation.ConstraintValidatorContext;
-
-// public class ValueOfEnumValidator implements ConstraintValidator<ValueOfEnum, CharSequence> {
-//     private List<String> acceptedValues;
-
-//     @Override
-//     public void initialize(ValueOfEnum annotation) {
-//         acceptedValues = Stream.of(annotation.enumClass().getEnumConstants())
-//                 .map(Enum::toString)
-//                 .collect(Collectors.toList());
-//     }
-
-//     @Override
-//     public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
-//         if (value == null) {
-//             return true;
-//         }
-
-//         return acceptedValues.contains(value.toString());
-//     }
-// }
-
 package com.loiane.enums.validation;
 
 import java.util.List;
